7fe15e18e361454260dee5d59edd6730f58fa2d6
Graeme Grant <gragra33@hotmail.com>
2023-05-19 18:52:29 +1000

    added ValidatorViewModelBase

---
+++ src/Blazing.Mvvm/ViewModels/ValidatorViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Blazing.Mvvm.ComponentModel;
+
+public abstract partial class ValidatorViewModelBase : ObservableValidator, IViewModelBase
+{
+    public virtual async Task OnInitializedAsync()
+        => await Loaded().ConfigureAwait(true);
+
+    protected virtual void NotifyStateChanged() => OnPropertyChanged((string?)null);
+
+    [RelayCommand]
+    public virtual async Task Loaded()
+        => await Task.CompletedTask.ConfigureAwait(false);
+}
