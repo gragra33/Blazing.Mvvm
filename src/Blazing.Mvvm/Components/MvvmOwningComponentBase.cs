09cd9a2c70fe999ad263d52cb9795a902518e1f6
Graeme Grant <gragra33@hotmail.com>
2024-01-29 13:32:36 +1100

    added OwningComponentBase support

---
+++ src/Blazing.Mvvm/Components/MvvmOwningComponentBase.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using Blazing.Mvvm.ComponentModel;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazing.Mvvm.Components;
+
+public abstract class MvvmOwningComponentBase<TViewModel> : OwningComponentBase, IView<TViewModel>, IAsyncDisposable where TViewModel : IViewModelBase
+{
+    private bool _disposed;
+
+    [Inject]
+    protected TViewModel? ViewModel { get; set; }
+
+    protected override void OnInitialized()
+    {
+        // Cause changes to the ViewModel to make Blazor re-render
+        ViewModel!.PropertyChanged += OnPropertyChanged;
+        base.OnInitialized();
+    }
+
+    private void OnPropertyChanged(object? o, PropertyChangedEventArgs propertyChangedEventArgs)
+        => InvokeAsync(StateHasChanged);
+
+    protected override Task OnInitializedAsync()
+        => ViewModel!.OnInitializedAsync();
+
+    protected override void Dispose(bool disposing)
+    {
+        if (_disposed)
+            return;
+        
+        if (disposing)
+            ViewModel!.PropertyChanged -= OnPropertyChanged;
+
+#if DEBUG
+        Console.WriteLine($"..Disposing: {GetType().FullName}");
+#endif
+        _disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        TViewModel? viewModel = ViewModel;
+
+        if(viewModel is IAsyncDisposable asyncDisposable)
+            await asyncDisposable.DisposeAsync();
+
+        if(viewModel is IDisposable disposable)
+            disposable.Dispose();
+        
+        Dispose();
+    }
+}
