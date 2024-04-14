1712d4cdafd14bab64eedb329ae521f94a6ef17b
Harry Kakoulidis <harry@uptiv.com>
2024-04-14 11:50:22 +0300

    Updates

--- src/Blazing.Mvvm.Base/IMvvmNavigationManager.cs
+++ src/Blazing.Mvvm.Base/IMvvmNavigationManager.cs
@@ -5,48 +5,58 @@
 /// </summary>
 public interface IMvvmNavigationManager
 {
-    /// <summary>
-    /// Navigates to the specified associated URI.
-    /// </summary>
-    /// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
-    /// <param name="forceLoad">If true, bypasses client-side routing and forces the browser to load the new page from the server, whether or not the URI would normally be handled by the client-side router.</param>
-    /// <param name="replace">If true, replaces the current entry in the history stack. If false, appends the new entry to the history stack.</param>
-    void NavigateTo<TViewModel>(bool? forceLoad = false, bool? replace = false)
-        where TViewModel : IViewModelBase;
+	/// <summary>
+	/// Navigates to the specified associated URI.
+	/// </summary>
+	/// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
+	void NavigateTo<TViewModel>() 
+		where TViewModel : IViewModelBase
+	{
+		NavigateTo<TViewModel>(false, false);
+	}
 
-    /// <summary>
-    /// Navigates to the specified associated URI.
-    /// </summary>
-    /// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
-    /// <param name="options">Provides additional <see cref="BrowserNavigationOptions"/>.</param>
-    void NavigateTo<TViewModel>(BrowserNavigationOptions options)
-        where TViewModel : IViewModelBase;
+	/// <summary>
+	/// Navigates to the specified associated URI.
+	/// </summary>
+	/// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
+	/// <param name="forceLoad">If true, bypasses client-side routing and forces the browser to load the new page from the server, whether or not the URI would normally be handled by the client-side router.</param>
+	/// <param name="replace">If true, replaces the current entry in the history stack. If false, appends the new entry to the history stack.</param>
+	void NavigateTo<TViewModel>(bool? forceLoad = false, bool? replace = false)
+		where TViewModel : IViewModelBase;
 
-    /// <summary>
-    /// Navigates to the specified associated URI.
-    /// </summary>
-    /// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
-    /// <param name="relativeUri">relative URI &/or QueryString appended to the navigation Uri.</param>
-    /// <param name="forceLoad">If true, bypasses client-side routing and forces the browser to load the new page from the server, whether or not the URI would normally be handled by the client-side router.</param>
-    /// <param name="replace">If true, replaces the current entry in the history stack. If false, appends the new entry to the history stack.</param>
-    void NavigateTo<TViewModel>(string? relativeUri = null, bool? forceLoad = false, bool? replace = false)
-        where TViewModel : IViewModelBase;
+	/// <summary>
+	/// Navigates to the specified associated URI.
+	/// </summary>
+	/// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
+	/// <param name="options">Provides additional <see cref="BrowserNavigationOptions"/>.</param>
+	void NavigateTo<TViewModel>(BrowserNavigationOptions options)
+		where TViewModel : IViewModelBase;
 
-    /// <summary>
-    /// Navigates to the specified associated URI.
-    /// </summary>
-    /// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
-    /// <param name="relativeUri">relative URI &/or QueryString appended to the navigation Uri.</param>
-    /// <param name="options">Provides additional <see cref="BrowserNavigationOptions"/>.</param>
-    void NavigateTo<TViewModel>(string relativeUri, BrowserNavigationOptions options)
-        where TViewModel : IViewModelBase;
+	/// <summary>
+	/// Navigates to the specified associated URI.
+	/// </summary>
+	/// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
+	/// <param name="relativeUri">relative URI &/or QueryString appended to the navigation Uri.</param>
+	/// <param name="forceLoad">If true, bypasses client-side routing and forces the browser to load the new page from the server, whether or not the URI would normally be handled by the client-side router.</param>
+	/// <param name="replace">If true, replaces the current entry in the history stack. If false, appends the new entry to the history stack.</param>
+	void NavigateTo<TViewModel>(string? relativeUri = null, bool? forceLoad = false, bool? replace = false)
+		where TViewModel : IViewModelBase;
 
-    /// <summary>
-    /// Get the <see cref="IViewModelBase"/> associated URI.
-    /// </summary>
-    /// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
-    /// <returns>A relative URI path.</returns>
-    /// <exception cref="ArgumentException"></exception>
-    string GetUri<TViewModel>()
-        where TViewModel : IViewModelBase;
+	/// <summary>
+	/// Navigates to the specified associated URI.
+	/// </summary>
+	/// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
+	/// <param name="relativeUri">relative URI &/or QueryString appended to the navigation Uri.</param>
+	/// <param name="options">Provides additional <see cref="BrowserNavigationOptions"/>.</param>
+	void NavigateTo<TViewModel>(string relativeUri, BrowserNavigationOptions options)
+		where TViewModel : IViewModelBase;
+
+	/// <summary>
+	/// Get the <see cref="IViewModelBase"/> associated URI.
+	/// </summary>
+	/// <typeparam name="TViewModel">The type <see cref="IViewModelBase"/> to use to determine the URI to navigate to.</typeparam>
+	/// <returns>A relative URI path.</returns>
+	/// <exception cref="ArgumentException"></exception>
+	string GetUri<TViewModel>()
+		where TViewModel : IViewModelBase;
 }
