25fb71d3f42a2fe5954b04b2e7533d6d2421605b
Graeme Grant <gragra33@hotmail.com>
2025-11-18 10:36:14 +1100

    removed old dupelicate sample projects - now in samples folder & updated for multi-target frameworks.

--- src/Blazing.Mvvm.Sample.Server/Data/IWeatherService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Blazing.Mvvm.Sample.Server.Models;
-
-namespace Blazing.Mvvm.Sample.Server.Data;
-
-public interface IWeatherService
-{
-    Task<IEnumerable<WeatherForecast>?> GetForecastAsync(CancellationToken cancellationToken = default);
-}
--- src/Blazing.Mvvm.Sample.Server/Models/Messages.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Blazing.Mvvm.Sample.Server.Models;
-
-public record ConvertHexToAsciiMessage(string HexToConvert);
-public record ConvertAsciiToHexMessage(string AsciiToConvert);
-public record ResetHexAsciiInputsMessage;
