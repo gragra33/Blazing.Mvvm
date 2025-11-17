74d12098a6922b8bb7cfd7a7cd74dacf7486fb73
Graeme Grant <gragra33@hotmail.com>
2025-11-18 09:58:50 +1100

    merge master branch in

---
+++ src/Blazing.Mvvm.Sample.Server/ViewModels/EditContactViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using Blazing.Mvvm.ComponentModel;
+using Blazing.Mvvm.Sample.Server.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Blazing.Mvvm.Sample.Server.ViewModels;
+
+public sealed partial class EditContactViewModel : ViewModelBase, IDisposable
+{
+    private readonly ILogger<EditContactViewModel> _logger;
+
+    [ObservableProperty]
+    private ContactInfo _contact = new();
+
+    public EditContactViewModel(ILogger<EditContactViewModel> logger)
+    {
+        _logger = logger;
+        Contact.PropertyChanged += ContactOnPropertyChanged;
+    }
+
+    public void Dispose()
+        => Contact.PropertyChanged -= ContactOnPropertyChanged;
+
+    [RelayCommand]
+    private void ClearForm()
+        => Contact = new ContactInfo();
+
+    [RelayCommand]
+    private void Save()
+        => _logger.LogInformation("Form is valid and submitted!");
+
+    private void ContactOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        => NotifyStateChanged();
+}
---
+++ src/Blazing.Mvvm.Sample.WebApp/Blazing.Mvvm.Sample.WebApp/Program.cs
@@ -0,0 +1,52 @@
+using Blazing.Mvvm;
+using Blazing.Mvvm.Sample.WebApp.Client.Data;
+using Blazing.Mvvm.Sample.WebApp.Components;
+using Blazing.Mvvm.Sample.WebApp.Data;
+using CommunityToolkit.Mvvm.Messaging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSingleton<IWeatherService, ServerWeatherService>();
+builder.Services.AddSingleton<IMessenger>(_ => WeakReferenceMessenger.Default);
+
+// Add Blazing.Mvvm
+builder.Services.AddMvvm(options =>
+{
+    options.HostingModelType = BlazorHostingModelType.WebApp;
+    options.ParameterResolutionMode = ParameterResolutionMode.ViewAndViewModel;
+    options.RegisterViewModelsFromAssemblyContaining<Blazing.Mvvm.Sample.WebApp.Client._Imports>();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Blazing.Mvvm.Sample.WebApp.Client._Imports).Assembly);
+
+app.MapGet("/api/weatherforecast", (IWeatherService weatherService, CancellationToken cancellationToken)
+    => weatherService.GetForecastAsync(cancellationToken));
+
+await app.RunAsync();
