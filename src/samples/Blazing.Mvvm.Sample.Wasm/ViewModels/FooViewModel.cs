a1e7717ac70160f243539e82f9c3065796ded0e6
Graeme Grant <gragra33@hotmail.com>
2025-12-07 12:20:31 +1100

    fixed analyzers not activating correctly on sample projects like with Blazing.Mvvm.Sample.Wasm project

---
+++ src/samples/Blazing.Mvvm.Sample.Wasm/ViewModels/FooViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Blazing.Mvvm.Sample.Wasm.ViewModels;
+
+public class FooViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private string? _name;
+    public string? Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+        }
+    }
+}
