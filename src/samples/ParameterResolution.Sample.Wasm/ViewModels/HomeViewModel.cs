9babdf4eac40f0e1ce0f9258ba5d82e023ed0c56
Graeme Grant <gragra33@hotmail.com>
2025-12-11 12:35:19 +1100

    updated app to have heach demo as an individual item on the navmenu; updated homepage to be more mvvm specific.

--- src/samples/ParameterResolution.Sample.Wasm/ViewModels/HomeViewModel.cs
+++ src/samples/ParameterResolution.Sample.Wasm/ViewModels/HomeViewModel.cs
@@ -1,7 +1,21 @@
 using Blazing.Mvvm.ComponentModel;
+using Blazing.Mvvm.Components;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ParameterResolution.Sample.Wasm.ViewModels;
 
-public sealed class HomeViewModel : ViewModelBase
+public sealed partial class HomeViewModel : ViewModelBase
 {
+    private readonly IMvvmNavigationManager _navigationManager;
+
+    public HomeViewModel(IMvvmNavigationManager navigationManager)
+    {
+        _navigationManager = navigationManager;
+    }
+
+    [RelayCommand]
+    private void NavigateWithParams(string queryString)
+    {
+        _navigationManager.NavigateTo<ParameterDemoViewModel>(queryString);
+    }
 }
